94e4c83768ab5126beed35d7346f7b574012307e
GarMamyan <141146744+GarMamyan@users.noreply.github.com>
2025-03-05 13:28:01 +0300

    Delete SQL.sql

--- SQL.sql
+++
@@ -1,8 +0,0 @@
-SELECT order_id ,
-       product_ids
-FROM   orders
-WHERE  order_id in (SELECT order_id
-                    FROM   courier_actions
-                    WHERE  action = 'deliver_order'
-                    ORDER BY time desc limit 100)
-ORDER BY order_id
